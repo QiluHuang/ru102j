b7b4bf7f1187e2da9df1204c899434bd6dccc604
Qilu Huang <hellolunahuang@gmail.com>
2025-02-21 22:29:37 -0800

    challenge#7 sliding window rate limiter implementation

--- src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -1,6 +1,11 @@
 package com.redislabs.university.RU102J.dao;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
+
+import static com.redislabs.university.RU102J.core.KeyHelper.getKey;
 
 public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
 
@@ -18,7 +23,26 @@ public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
     // Challenge #7
     @Override
     public void hit(String name) throws RateLimitExceededException {
-        // START CHALLENGE #7
-        // END CHALLENGE #7
+        try (Jedis jedis = jedisPool.getResource()) {
+            // key of the sorted set: [limiter]:[windowSize]:[name]:[maxHits]
+            String key = getKey("limiter:" + windowSizeMS + ":" + name + ":" + maxHits);
+
+            // get the current time in millisecond as score for current hit and the [timestamp]-[random-number]
+            long currentTimeMillis = System.currentTimeMillis();
+            String member = currentTimeMillis + ":" + Math.random();
+
+
+            // Add current value, remove expired value, and get the total number of elements in the sorted set using Redis transaction
+            Transaction transaction = jedis.multi();
+            transaction.zadd(key, currentTimeMillis, member);
+            transaction.zremrangeByScore(key, 0, currentTimeMillis - windowSizeMS);
+            Response<Long> hits = transaction.zcard(key);
+            transaction.exec();
+
+            // throw exception if exceed the limit
+            if (hits.get() > maxHits) {
+                throw new RateLimitExceededException();
+            }
+        }
     }
 }
